72c72fc83f9a54b3c955a2cbdef6bb72f4c12d14
Poorva Ramani <poorva.ramani@washpost.com>
2019-12-07 13:56:51 -0600

    uploaded video

--- index.js
+++ index.js
@@ -12,17 +12,6 @@ function generateHTML(data, colour, starCount) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script> 
     <style>
     .container{
       margin-right: 0;
@@ -31,38 +20,46 @@ function generateHTML(data, colour, starCount) {
     .card-body{
       background-color: ${colour};
       color:white;
+      box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+      padding: 10px;
+      margin: 20px;
+      text-align: center;
     }
     a{
       cursor:pointer;
       text-decoration: none;
       color:white;
+      padding:10px;
     }
     img{
       width: 200px;
       height: 200px;
     }
+    .bio{
+      text-align: center;
+    }
     </style> 
     <title>Document</title>
   </head>
     <body>
-      <div class="container bg-light">
+      <div class="container">
       <!--main card body-->
-          <div class="row text-center">
-          <div class="col-lg-9 card-body d-block border shadow rounded-lg m-3">
-                  <img class="rounded-lg justify-content-center p-2" src="${data.avatar_url}" alt="...">
+          <div class="row">
+          <div class="col-lg-9 card-body">
+                  <img src="${data.avatar_url}" alt="...">
                   <h2> Hi! </h2>
                   <h2> My name is ${data.name}</h2>
                   <h5> Currently @ ${data.company}</h5>
                   <h6> 
-                    <a class="p-3"  href="https://www.google.com/maps/place/${data.location}" target="_blank"><svg id="i-location" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                    <a href="https://www.google.com/maps/place/${data.location}" target="_blank"><svg id="i-location" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
                       <circle cx="16" cy="11" r="4" />
                       <path d="M24 15 C21 22 16 30 16 30 16 30 11 22 8 15 5 8 10 2 16 2 22 2 27 8 24 15 Z" />
                   </svg> ${data.location}</a>
-                    <a class="p-3" href=${data.html_url} target="_blank">
+                    <a href=${data.html_url} target="_blank">
                       <svg id="i-github" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="32" height="32">
                         <path stroke-width="0" fill="currentColor" d="M32 0 C14 0 0 14 0 32 0 53 19 62 22 62 24 62 24 61 24 60 L24 55 C17 57 14 53 13 50 13 50 13 49 11 47 10 46 6 44 10 44 13 44 15 48 15 48 18 52 22 51 24 50 24 48 26 46 26 46 18 45 12 42 12 31 12 27 13 24 15 22 15 22 13 18 15 13 15 13 20 13 24 17 27 15 37 15 40 17 44 13 49 13 49 13 51 20 49 22 49 22 51 24 52 27 52 31 52 42 45 45 38 46 39 47 40 49 40 52 L40 60 C40 61 40 62 42 62 45 62 64 53 64 32 64 14 50 0 32 0 Z" />
                     </svg> Git</a>
-                    <a class="p-3" href=${data.blog} target="_blank">
+                    <a href=${data.blog} target="_blank">
                       <svg id="i-feed" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
                         <circle cx="6" cy="26" r="2" fill="currentColor" />
                         <path d="M4 15 C11 15 17 21 17 28 M4 6 C17 6 26 15 26 28" />
@@ -70,25 +67,25 @@ function generateHTML(data, colour, starCount) {
                   </h6>
               </div>
           </div>
-          <div class="row text-center">
+          <div class="row bio">
             <h3>${data.bio}</h3>
           </div>
           <div class="row text-center">
-              <div class="col-lg-4 card-body d-block border shadow rounded-lg m-3">
+              <div class="col-lg-4 card-body">
                 <h2>Public Respositories</h2>
                 <h4>${data.public_repos}</h4>
               </div>
-              <div class="col-lg-4 card-body d-block border shadow rounded-lg m-3">
+              <div class="col-lg-4 card-body">
                   <h2>Followers</h2>
                   <h4>${data.followers}</h4>
                 </div> 
           </div>
-          <div class="row text-center">
-              <div class="col-lg-4 card-body d-block border shadow rounded-lg m-3">
+          <div class="row">
+              <div class="col-lg-4 card-body">
                 <h2>Github Stars</h2>
                 <h4>${starCount}</h4>
               </div>
-              <div class="col-lg-4 card-body d-block border shadow rounded-lg m-3">
+              <div class="col-lg-4 card-body">
                   <h2>Following</h2>
                   <h4>${data.following}</h4>
                 </div> 
